e0ea67d2c3e6f9d400c14346b71f43a8d3c1f411
GeniusLuo <505101092@qq.com>
2019-06-17 21:56:48 +0800

    animation

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,13 +1,29 @@
 import React, {Component, Fragment} from 'react'
+import './style.css'
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      show: true
+    }
+    this.handleToggle = this.handleToggle.bind(this)
+  }
+
+
   render() {
     return (
       <Fragment>
-        <div>hello</div>
-        <button>toggle</button>
+        <div className={this.state.show ? 'show' : 'hide'}>hello</div>
+        <button onClick={this.handleToggle}>toggle</button>
       </Fragment>
     )
+  }
+
+  handleToggle() {
+    this.setState({
+      show: !this.state.show
+    })
   }
 }
 
