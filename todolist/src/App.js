4086902f18f6852c1c89ca20ac99a52d28dc5529
GeniusLuo <505101092@qq.com>
2019-06-18 14:38:30 +0800

    animation

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,28 +1,46 @@
 import React, {Component, Fragment} from 'react'
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import './style.css'
 
 class App extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      show: true
+      list: []
     }
-    this.handleToggle = this.handleToggle.bind(this)
+    this.handleAddItem = this.handleAddItem.bind(this)
   }
 
 
   render() {
     return (
       <Fragment>
-        <div className={this.state.show ? 'show' : 'hide'}>hello</div>
-        <button onClick={this.handleToggle}>toggle</button>
+        <TransitionGroup>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <CSSTransition timeout={500}
+                               classNames="fade"
+                               unmountOnExit
+                               appear={true}
+                               key={index}
+                               onEntered={el => el.style.color = 'blue'}>
+                  <div>{item}</div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
+        <button onClick={this.handleAddItem}>toggle</button>
       </Fragment>
     )
   }
 
-  handleToggle() {
-    this.setState({
-      show: !this.state.show
+  handleAddItem() {
+    this.setState((prevState) => {
+      return {
+        list: [...prevState.list, 'item']
+      }
     })
   }
 }
